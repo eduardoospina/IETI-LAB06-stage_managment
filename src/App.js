8622aa6c2430f687b11c7cfe5249e25639de23ed
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-10-16 12:24:35 -0500

    parte 2 adelanto

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import React from 'react';
 import Login from "./Components/Login";
 import PaginaInicio from './Components/PaginaInicio';
 import {Routes, Route} from 'react-router-dom'
+import { themeReducer, initialState } from "./utils";
+import { ThemeContext } from "./ThemeContext";
 
 function App (){
+  const [state, dispatch] = useReducer(themeReducer, initialState);
   return (
-      <Routes>
-        <Route path = '/' element = {<Login/>} />
-        <Route path = '/Login' element = {<Login/>} />
-        <Route path = '/paginainicio' element = {<PaginaInicio/>} />
-      </Routes>
+    <ThemeContext.Provider  value={{ state, dispatch }}>
+        <Routes>
+          <Route path = '/' element = {<Login/>} />
+          <Route path = '/Login' element = {<Login/>} />
+          <Route path = '/paginainicio' element = {<PaginaInicio/>} />
+        </Routes>
+      </ThemeContext.Provider>
   )
 }
 
